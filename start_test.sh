cab58a2816a76929b194d18091f982dab1d5a568
lyapin <lajpinkonstantin@gmail.com>
2024-09-23 14:23:14 +0300

    tic tac toe

--- start_test.sh
+++ start_test.sh
@@ -4,6 +4,6 @@ sudo docker compose up;
 sudo docker stop $(sudo docker ps -a -q);
 sudo docker rm $(sudo docker ps -a -q);
 sudo docker rmi $(sudo docker images --format="{{.Repository}} {{.ID}}" |
-                  grep "^backend_for_bike_rental_service_picasso_diagnostic-web" | cut -d' ' -f2);
-sudo docker network rm backend_for_bike_rental_service_picasso_diagnostic_backend;
+                  grep "^simpl_react_frontend-web" | cut -d' ' -f2);
+sudo docker network rm simpl_react_frontend_frontend;
 sudo rm -r dump/;
